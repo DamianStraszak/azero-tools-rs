cf1a4e9e1311316ce68addb38e583fb609d6305a
Damian Straszak <damian.straszak@gmail.com>
2024-02-24 14:05:50 +0100

    Updating ink-wrapper

--- src/psp22/psp22_wrapper.rs
+++ src/psp22/psp22_wrapper.rs
@@ -1,14 +1,11 @@
 use codec::Encode as _;
-
-// This file was auto-generated with ink-wrapper (https://crates.io/crates/ink-wrapper).
-
 #[allow(dead_code)]
 pub const CODE_HASH: [u8; 32] = [
-    131, 184, 249, 211, 41, 15, 253, 47, 57, 129, 70, 40, 141, 30, 220, 53, 236, 74, 24, 168, 96,
-    112, 178, 40, 142, 124, 174, 30, 12, 163, 166, 151,
+    131u8, 184u8, 249u8, 211u8, 41u8, 15u8, 253u8, 47u8, 57u8, 129u8, 70u8, 40u8, 141u8, 30u8,
+    220u8, 53u8, 236u8, 74u8, 24u8, 168u8, 96u8, 112u8, 178u8, 40u8, 142u8, 124u8, 174u8, 30u8,
+    12u8, 163u8, 166u8, 151u8,
 ];
-
-#[derive(Debug, Clone, PartialEq, Eq, codec::Encode, codec::Decode)]
+#[derive(Debug, Clone, PartialEq, Eq, codec :: Encode, codec :: Decode)]
 pub enum PSP22Error {
     Custom(String),
     InsufficientBalance(),
@@ -17,20 +14,17 @@ pub enum PSP22Error {
     ZeroSenderAddress(),
     SafeTransferCheckFailed(String),
 }
-
-#[derive(Debug, Clone, PartialEq, Eq, codec::Encode, codec::Decode)]
+#[derive(Debug, Clone, PartialEq, Eq, codec :: Encode, codec :: Decode)]
 pub enum NoChainExtension {}
-
 pub mod event {
     #[allow(dead_code, clippy::large_enum_variant)]
-    #[derive(Debug, Clone, PartialEq, Eq, codec::Encode, codec::Decode)]
+    #[derive(Debug, Clone, PartialEq, Eq, codec :: Encode, codec :: Decode)]
     pub enum Event {
         Approval {
             owner: ink_primitives::AccountId,
             spender: ink_primitives::AccountId,
             amount: u128,
         },
-
         Transfer {
             from: Option<ink_primitives::AccountId>,
             to: Option<ink_primitives::AccountId>,
@@ -38,72 +32,25 @@ pub mod event {
         },
     }
 }
-
 #[derive(Debug, Clone, Copy)]
 pub struct Instance {
     account_id: ink_primitives::AccountId,
 }
-
 impl From<ink_primitives::AccountId> for Instance {
     fn from(account_id: ink_primitives::AccountId) -> Self {
         Self { account_id }
     }
 }
-
 impl From<Instance> for ink_primitives::AccountId {
     fn from(instance: Instance) -> Self {
         instance.account_id
     }
 }
-
 impl ink_wrapper_types::EventSource for Instance {
     type Event = event::Event;
 }
 
-#[async_trait::async_trait]
-pub trait PSP22Metadata {
-    fn token_name(
-        &self,
-    ) -> ink_wrapper_types::ReadCall<Result<Option<String>, ink_wrapper_types::InkLangError>>;
-    fn token_symbol(
-        &self,
-    ) -> ink_wrapper_types::ReadCall<Result<Option<String>, ink_wrapper_types::InkLangError>>;
-    fn token_decimals(
-        &self,
-    ) -> ink_wrapper_types::ReadCall<Result<u8, ink_wrapper_types::InkLangError>>;
-}
-
-#[async_trait::async_trait]
-impl PSP22Metadata for Instance {
-    ///  Returns the token name.
-    #[allow(dead_code, clippy::too_many_arguments)]
-    fn token_name(
-        &self,
-    ) -> ink_wrapper_types::ReadCall<Result<Option<String>, ink_wrapper_types::InkLangError>> {
-        let data = vec![61, 38, 27, 212];
-        ink_wrapper_types::ReadCall::new(self.account_id, data)
-    }
-
-    ///  Returns the token symbol.
-    #[allow(dead_code, clippy::too_many_arguments)]
-    fn token_symbol(
-        &self,
-    ) -> ink_wrapper_types::ReadCall<Result<Option<String>, ink_wrapper_types::InkLangError>> {
-        let data = vec![52, 32, 91, 229];
-        ink_wrapper_types::ReadCall::new(self.account_id, data)
-    }
-
-    ///  Returns the token decimals.
-    #[allow(dead_code, clippy::too_many_arguments)]
-    fn token_decimals(
-        &self,
-    ) -> ink_wrapper_types::ReadCall<Result<u8, ink_wrapper_types::InkLangError>> {
-        let data = vec![114, 113, 183, 130];
-        ink_wrapper_types::ReadCall::new(self.account_id, data)
-    }
-}
-
-#[async_trait::async_trait]
+#[allow(dead_code)]
 pub trait PSP22 {
     fn total_supply(
         &self,
@@ -122,54 +69,50 @@ pub trait PSP22 {
         to: ink_primitives::AccountId,
         value: u128,
         _data: Vec<u8>,
-    ) -> ink_wrapper_types::ExecCall;
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>;
     fn transfer_from(
         &self,
         from: ink_primitives::AccountId,
         to: ink_primitives::AccountId,
         value: u128,
         _data: Vec<u8>,
-    ) -> ink_wrapper_types::ExecCall;
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>;
     fn approve(
         &self,
         spender: ink_primitives::AccountId,
         value: u128,
-    ) -> ink_wrapper_types::ExecCall;
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>;
     fn increase_allowance(
         &self,
         spender: ink_primitives::AccountId,
         delta_value: u128,
-    ) -> ink_wrapper_types::ExecCall;
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>;
     fn decrease_allowance(
         &self,
         spender: ink_primitives::AccountId,
         delta_value: u128,
-    ) -> ink_wrapper_types::ExecCall;
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>;
 }
-
-#[async_trait::async_trait]
 impl PSP22 for Instance {
     #[allow(dead_code, clippy::too_many_arguments)]
     fn total_supply(
         &self,
     ) -> ink_wrapper_types::ReadCall<Result<u128, ink_wrapper_types::InkLangError>> {
-        let data = vec![22, 45, 248, 194];
+        let data = vec![22u8, 45u8, 248u8, 194u8];
         ink_wrapper_types::ReadCall::new(self.account_id, data)
     }
-
     #[allow(dead_code, clippy::too_many_arguments)]
     fn balance_of(
         &self,
         owner: ink_primitives::AccountId,
     ) -> ink_wrapper_types::ReadCall<Result<u128, ink_wrapper_types::InkLangError>> {
         let data = {
-            let mut data = vec![101, 104, 56, 47];
+            let mut data = vec![101u8, 104u8, 56u8, 47u8];
             owner.encode_to(&mut data);
             data
         };
         ink_wrapper_types::ReadCall::new(self.account_id, data)
     }
-
     #[allow(dead_code, clippy::too_many_arguments)]
     fn allowance(
         &self,
@@ -177,23 +120,23 @@ impl PSP22 for Instance {
         spender: ink_primitives::AccountId,
     ) -> ink_wrapper_types::ReadCall<Result<u128, ink_wrapper_types::InkLangError>> {
         let data = {
-            let mut data = vec![77, 71, 217, 33];
+            let mut data = vec![77u8, 71u8, 217u8, 33u8];
             owner.encode_to(&mut data);
             spender.encode_to(&mut data);
             data
         };
         ink_wrapper_types::ReadCall::new(self.account_id, data)
     }
-
     #[allow(dead_code, clippy::too_many_arguments)]
     fn transfer(
         &self,
         to: ink_primitives::AccountId,
         value: u128,
         _data: Vec<u8>,
-    ) -> ink_wrapper_types::ExecCall {
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>
+    {
         let data = {
-            let mut data = vec![219, 32, 249, 245];
+            let mut data = vec![219u8, 32u8, 249u8, 245u8];
             to.encode_to(&mut data);
             value.encode_to(&mut data);
             _data.encode_to(&mut data);
@@ -201,7 +144,6 @@ impl PSP22 for Instance {
         };
         ink_wrapper_types::ExecCall::new(self.account_id, data)
     }
-
     #[allow(dead_code, clippy::too_many_arguments)]
     fn transfer_from(
         &self,
@@ -209,9 +151,10 @@ impl PSP22 for Instance {
         to: ink_primitives::AccountId,
         value: u128,
         _data: Vec<u8>,
-    ) -> ink_wrapper_types::ExecCall {
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>
+    {
         let data = {
-            let mut data = vec![84, 179, 199, 110];
+            let mut data = vec![84u8, 179u8, 199u8, 110u8];
             from.encode_to(&mut data);
             to.encode_to(&mut data);
             value.encode_to(&mut data);
@@ -220,45 +163,45 @@ impl PSP22 for Instance {
         };
         ink_wrapper_types::ExecCall::new(self.account_id, data)
     }
-
     #[allow(dead_code, clippy::too_many_arguments)]
     fn approve(
         &self,
         spender: ink_primitives::AccountId,
         value: u128,
-    ) -> ink_wrapper_types::ExecCall {
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>
+    {
         let data = {
-            let mut data = vec![178, 15, 27, 189];
+            let mut data = vec![178u8, 15u8, 27u8, 189u8];
             spender.encode_to(&mut data);
             value.encode_to(&mut data);
             data
         };
         ink_wrapper_types::ExecCall::new(self.account_id, data)
     }
-
     #[allow(dead_code, clippy::too_many_arguments)]
     fn increase_allowance(
         &self,
         spender: ink_primitives::AccountId,
         delta_value: u128,
-    ) -> ink_wrapper_types::ExecCall {
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>
+    {
         let data = {
-            let mut data = vec![150, 214, 181, 122];
+            let mut data = vec![150u8, 214u8, 181u8, 122u8];
             spender.encode_to(&mut data);
             delta_value.encode_to(&mut data);
             data
         };
         ink_wrapper_types::ExecCall::new(self.account_id, data)
     }
-
     #[allow(dead_code, clippy::too_many_arguments)]
     fn decrease_allowance(
         &self,
         spender: ink_primitives::AccountId,
         delta_value: u128,
-    ) -> ink_wrapper_types::ExecCall {
+    ) -> ink_wrapper_types::ExecCall<Result<Result<(), PSP22Error>, ink_wrapper_types::InkLangError>>
+    {
         let data = {
-            let mut data = vec![254, 203, 87, 213];
+            let mut data = vec![254u8, 203u8, 87u8, 213u8];
             spender.encode_to(&mut data);
             delta_value.encode_to(&mut data);
             data
@@ -266,12 +209,48 @@ impl PSP22 for Instance {
         ink_wrapper_types::ExecCall::new(self.account_id, data)
     }
 }
-
+pub trait PSP22Metadata {
+    fn token_name(
+        &self,
+    ) -> ink_wrapper_types::ReadCall<Result<Option<String>, ink_wrapper_types::InkLangError>>;
+    fn token_symbol(
+        &self,
+    ) -> ink_wrapper_types::ReadCall<Result<Option<String>, ink_wrapper_types::InkLangError>>;
+    fn token_decimals(
+        &self,
+    ) -> ink_wrapper_types::ReadCall<Result<u8, ink_wrapper_types::InkLangError>>;
+}
+impl PSP22Metadata for Instance {
+    #[doc = "Returns the token name."]
+    #[allow(dead_code, clippy::too_many_arguments)]
+    fn token_name(
+        &self,
+    ) -> ink_wrapper_types::ReadCall<Result<Option<String>, ink_wrapper_types::InkLangError>> {
+        let data = vec![61u8, 38u8, 27u8, 212u8];
+        ink_wrapper_types::ReadCall::new(self.account_id, data)
+    }
+    #[doc = "Returns the token symbol."]
+    #[allow(dead_code, clippy::too_many_arguments)]
+    fn token_symbol(
+        &self,
+    ) -> ink_wrapper_types::ReadCall<Result<Option<String>, ink_wrapper_types::InkLangError>> {
+        let data = vec![52u8, 32u8, 91u8, 229u8];
+        ink_wrapper_types::ReadCall::new(self.account_id, data)
+    }
+    #[doc = "Returns the token decimals."]
+    #[allow(dead_code, clippy::too_many_arguments)]
+    fn token_decimals(
+        &self,
+    ) -> ink_wrapper_types::ReadCall<Result<u8, ink_wrapper_types::InkLangError>> {
+        let data = vec![114u8, 113u8, 183u8, 130u8];
+        ink_wrapper_types::ReadCall::new(self.account_id, data)
+    }
+}
 impl Instance {
     #[allow(dead_code, clippy::too_many_arguments)]
     pub fn new(supply: u128) -> ink_wrapper_types::InstantiateCall<Self> {
         let data = {
-            let mut data = vec![155, 174, 157, 94];
+            let mut data = vec![155u8, 174u8, 157u8, 94u8];
             supply.encode_to(&mut data);
             data
         };
